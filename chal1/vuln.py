53224912e68d1b6826f639bffa298439d7e9c92f
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-11-25 11:41:11 +0100

    disable debug printing of udp flag

--- chal1/vuln.py
+++ chal1/vuln.py
@@ -16,10 +16,10 @@ print("You are ready? Transmitting flag now!")
 
 MESSAGE = b"FLAG_PLACEHOLDER"
 
-print("UDP target IP: %s" % UDP_IP)
-print("UDP target port: %s" % UDP_PORT)
-print("message: %s" % MESSAGE)
-  
+# print("UDP target IP: %s" % UDP_IP)
+# print("UDP target port: %s" % UDP_PORT)
+# print("message: %s" % MESSAGE)
+
 sock = socket.socket(socket.AF_INET, # Internet
                     socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
